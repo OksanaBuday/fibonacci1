658b6790665f49f83d7c52f88690582d0b0f1dd1
Oksana Buday <oksanabuday@gmail.com>
2017-06-29 13:13:43 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ var n = Number(prompt('Введіть n (рекурсія)'));
 function fib(n) {
     var a = 1;
     var b = 1;
-    for (var i = 2; i < n; i++) {
+    for (var i = 3; i < n; i++) {
         var c = a + b;
         a = b;
         b = c;
